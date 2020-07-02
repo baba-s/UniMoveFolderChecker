24be3067edddd71b92c3d74f6164ec79cb67280b
baba_s <shotababa1206@gmail.com>
2020-07-02 09:59:35 +0900

    first commit

---
+++ Editor/MoveFolderChecker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class MoveFolderChecker : UnityEditor.AssetModificationProcessor
+	{
+		private static AssetMoveResult OnWillMoveAsset( string sourcePath, string destinationPath )
+		{
+			var isFolder = AssetDatabase.IsValidFolder( sourcePath );
+
+			if ( !isFolder ) return AssetMoveResult.DidNotMove;
+
+			var message = $"{sourcePath} フォルダを\n{destinationPath} に移動しますか？";
+
+			var isOk = EditorUtility.DisplayDialog
+			(
+				title: "フォルダ移動",
+				message: message,
+				ok: "OK",
+				cancel: "キャンセル"
+			);
+
+			return isOk ? AssetMoveResult.DidNotMove : AssetMoveResult.FailedMove;
+		}
+	}
+}
